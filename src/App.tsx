891ba8c39750444904ba84947adbdb3c907d07a7
daeyeub <97010621+daeyeub@users.noreply.github.com>
2022-08-27 16:56:33 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,28 +6,41 @@ const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
-  flex-direction: column;
 `;
 const Box = styled(motion.div)`
   width: 400px;
-  height: 200px;
+  height: 400px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  display: felx;
+  display: flex;
   justify-content: center;
-  font-size: 30px;
   align-items: center;
-  position: absolute;
-  top: 50px;
+`;
+const Circle = styled(motion.div)`
+  background-color: #005aff;
+  width: 100px;
+  height: 100px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => setClicked((prev) => !prev);
   return (
-    <Wrapper>
-      <Box></Box>
+    <Wrapper onClick={toggleClicked}>
+      <Box>
+        {!clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: 50, scale: 2 }} />
+        ) : null}
+      </Box>
+      <Box>
+        {clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: 0, scale: 0.5 }} />
+        ) : null}
+      </Box>
     </Wrapper>
   );
 }
